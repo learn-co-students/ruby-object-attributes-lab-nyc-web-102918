284f48f5da6ff9e6f7dcf86a3ff2ef4e84b913f1
demiansims <demiansims@gmail.com>
2018-10-30 14:50:16 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,28 @@
+class Dog
+
+  # def initialize(name, breed)
+  #
+  def name=(new_name)
+
+    @name = new_name
+
+  end
+
+  def name
+
+    @name
+  end
+
+  def breed=(breed_type)
+
+    @breed = breed_type
+
+  end
+
+  def breed
+
+    @breed
+
+  end
+
+end
